2df394b6afb91797a591ecf54c85bc0efa430d61
Carlos Junior <carlosjunior866@gmail.com>
2022-08-23 22:18:34 +0100

    added

--- c06piscine/ex00/ft_print_program_name.c
+++ c06piscine/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: carloda- <carloda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 16:29:08 by carloda-          #+#    #+#             */
-/*   Updated: 2022/08/23 19:08:28 by carloda-         ###   ########.fr       */
+/*   Updated: 2022/08/23 19:58:03 by carloda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ int main (int argc, char **argv)
     int i;
 
     i = 0;
-    printf("Numero de Argumentos: %d\n", argc);
     while (argv[0][i] != '\0')
     {
-        printf("%c", argv[1][i]);
+        printf("%c", argv[0][i]);
         i++;
     }
     return (0);
