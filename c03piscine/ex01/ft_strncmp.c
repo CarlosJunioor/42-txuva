4dc91ebc7f528c809379282d4e218c8e8e805b3a
Carlos Junior <carlosjunior866@gmail.com>
2022-08-24 18:28:33 +0100

    c06 working

--- c03piscine/ex01/ft_strncmp.c
+++ c03piscine/ex01/ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: carloda- <carloda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 19:00:31 by carloda-          #+#    #+#             */
-/*   Updated: 2022/08/23 13:47:03 by carloda-         ###   ########.fr       */
+/*   Updated: 2022/08/24 17:01:09 by carloda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	count;
+	unsigned int count;
 
 	count = 0;
 	while (s1[count] == s2[count] && s1[count] != '\0'
--- c06piscine/ex01/ft_print_params.c
+++ c06piscine/ex01/ft_print_params.c
@@ -6,16 +6,29 @@
 /*   By: carloda- <carloda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 19:58:19 by carloda-          #+#    #+#             */
-/*   Updated: 2022/08/23 19:59:58 by carloda-         ###   ########.fr       */
+/*   Updated: 2022/08/24 18:25:51 by carloda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 
-int main ()
+int main(int argc, char **argv)
 {
     int i;
+    int x;
 
     i = 0;
-    while (argv)
+    x = 1;
+    while (x < argc)
+    {
+        i = 0;
+        printf(" ");
+            while (argv[x][i] != '\0')
+            {
+                printf("%c", argv[x][i]);
+                i++; 
+            }
+        x++;
+    } 
+    return (0);
 }
