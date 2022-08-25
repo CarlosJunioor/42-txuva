331fe392b5c36c7e510e062941bbf8635d0138a8
Carlos Junior <carlosjunior866@gmail.com>
2022-08-25 21:28:15 +0100

    added

---
+++ Practice/aff_z.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_z.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carloda- <carloda-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/25 16:14:36 by carloda-          #+#    #+#             */
+/*   Updated: 2022/08/25 16:30:57 by carloda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include<stdio.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void    ft_print_numbers(void)
+{
+    char n;
+
+    n = 0;
+    while(n <= '9')
+    {
+        ft_putchar(n);
+        n++;
+    }
+}
+
+int main()
+{
+    ft_print_numbers();
+    return (0);
+}
