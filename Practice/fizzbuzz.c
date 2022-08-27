f15f4cd5b463fd46797c696dfdb24e3fa36472d8
CarlosJunioor <carlosjunior866@gmail.com>
2022-08-27 23:04:08 +0100

    dsfsd

--- Practice/fizzbuzz.c
+++ Practice/fizzbuzz.c
@@ -3,52 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   fizzbuzz.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/26 20:18:56 by marvin            #+#    #+#             */
-/*   Updated: 2022/08/26 20:18:56 by marvin           ###   ########.fr       */
-=======
-/*   By: carloda- <carloda-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 20:07:30 by carloda-          #+#    #+#             */
-/*   Updated: 2022/08/25 22:00:36 by carloda-         ###   ########.fr       */
+/*   Created: 2022/08/27 22:40:31 by marvin            #+#    #+#             */
+/*   Updated: 2022/08/27 22:40:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-<<<<<<< HEAD
-void ft_write_number(int number)
+void ft_write_numbers(int numbers)
 {
-    if (number > 9)
-            ft_write_number(number / 10);
-            write(1, &"0123456789"[number % 10], 1);
+
 }
 
-int main (void)
-    int number;
+int main ()
+{
     
-    number = 1;
-    while (number <= 100)
-        write(1, "a", 1);
-void    ft_write_number(int number)
-{
-    if (number > 9)
-        ft_write_number(number / 10);
-        write(1, &"0123456789"[number % 10], 1);
-}
-
-int main(void)
-{
-    int number;
-
-    number = 1;
-    while (number <= 100)
-    {
-        if (number == )
-        ft_write_number(number);
-        write(1, "\n", 1);
-        number++;
-    }
 }
